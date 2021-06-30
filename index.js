ed7934ef8cf0373ff1f0244f77888fffed2d2669
Jarad Solomon <jaradsolomon@gmail.com>
2021-06-30 17:24:33 -0500

    commit tree 38c752bcd8181c4fcd77d7059fabac5938f106a7

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+function createEmployeeRecord([first, last, role, n]){
+
+    
+    const rec = {firstName : first, familyName : last, title : role, payPerHour : n, timeInEvents : [], timeOutEvents : []}
+
+    return rec
+}
+
+
+function createEmployeeRecords(arrayOfArrays){
+
+const records = [arrayOfArrays[1], arrayOfArrays[2]]
+
+records.forEach( arr => {
+
+    const recordsObject = Object.assign({}, arr)
+
+    recordsObject['1'] = recordsObject['firstName']
+
+})
+
+
+
+
+
+
+
+// records.map( x => { 
+//     return {firstName : x[1]}
+// })
+
+//const withNames = createEmployeeRecord(records)
+
+return records
+    
+}
